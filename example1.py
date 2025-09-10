9254e908dd73976ee97d2721d98f951f6cca520f
SedovAV <72596005+SedovAV@users.noreply.github.com>
2025-09-10 08:47:23 +0500

    Create example1.py

---
+++ example1.py
@@ -0,0 +1,57 @@
+from PyQt6.QtCore import QMimeData, Qt
+from PyQt6.QtGui import QDrag, QPixmap
+from PyQt6.QtWidgets import QApplication, QHBoxLayout, QPushButton, QWidget
+
+
+class DragButton(QPushButton):
+   def mouseMoveEvent(self, e):
+      if e.buttons() == Qt.MouseButton.LeftButton:
+         drag = QDrag(self)
+         mime = QMimeData()
+         drag.setMimeData(mime)
+
+         pixmap = QPixmap(self.size())
+         self.render(pixmap)
+         drag.setPixmap(pixmap)
+
+         drag.exec(Qt.DropAction.MoveAction)
+class Window(QWidget):
+   def __init__(self):
+      super().__init__()
+      self.setAcceptDrops(True)
+
+      self.blayout = QHBoxLayout()
+      for l in ["A", "B", "C", "D"]:
+         btn = DragButton(l)
+         self.blayout.addWidget(btn)
+
+      self.setLayout(self.blayout)
+
+   def dragEnterEvent(self, e):
+      e.accept()
+
+   def dropEvent(self, e):
+      pos = e.position()
+      widget = e.source()
+      self.blayout.removeWidget(widget)
+
+      for n in range(self.blayout.count()):
+         # Get the widget at each index in turn.
+         w = self.blayout.itemAt(n).widget()
+         if pos.x() < w.x() + w.size().width() // 2:
+            # We didn't drag past this widget.
+            # insert to the left of it.
+            break
+         else:
+            # We aren't on the left hand side of any widget,
+            # so we're at the end. Increment 1 to insert after.
+            n += 1
+
+      self.blayout.insertWidget(n, widget)
+      e.accept()
+
+app = QApplication([])
+w = Window()
+w.show()
+
+app.exec()
